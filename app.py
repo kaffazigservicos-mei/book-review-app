b2c99720f8c8274c9138fc741e64676dbc110e01
kaffazigservicos-mei <kaffa.zig.servicos@gmail.com>
2026-04-21 18:04:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import plotly.express as px
 st.set_page_config(layout="wide")
 
 df_reviews = pd.read_csv("datasets/customer reviews.csv")
-df_top100_books = pd.read_csv("datasets/Top-100 Trending books.csv")
+df_top100_books = pd.read_csv("datasets/Top-100 Trending Books.csv")
 
 price_max= df_top100_books["book price"].max()
 price_min= df_top100_books["book price"].min()
